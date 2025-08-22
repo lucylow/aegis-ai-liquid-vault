2b3645ba1a9df3731143a22532beb9f71db9e8e1
づ｡◕‿◕｡)づ lucylow <llow@edu.uwaterloo.ca>
2025-08-22 12:39:25 -0400

    change name on tab

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -120,7 +120,7 @@ const Layout = () => {
               </span>
             </div>
             <span className="text-xs text-gray-400 ml-11">
-              AEGIS Cross-Chain Lending
+              Cross-Chain Lending
             </span>
           </div>
           <button
